213e3ca1a5eeba33c0cad82eb9d75351f8a18d98
Pamela Macbook <pamela@cedros.dev>
2026-08-14 17:42:15 -0300

    created express server

--- src/renderer.ts
+++ src/renderer.ts
@@ -26,7 +26,7 @@
  * ```
  */
 
-import './index.css';
+import "./main.css";
 
 console.log(
   '👋 This message is being logged by "renderer.ts", included via Vite',
--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,4 +1,34 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config
 export default defineConfig({});
+
+// import { mkdirSync, readdirSync, copyFileSync } from "node:fs";
+// import { join, resolve } from "node:path";
+// import { defineConfig, type Plugin } from "vite";
+
+// const copyViews = (): Plugin => ({
+//   name: "copy-backend-views",
+//   writeBundle(options) {
+//     return;
+//     if (!options.dir) {
+//       return;
+//     }
+
+//     const sourceDirectory = resolve(__dirname, "src/backend/views");
+//     const targetDirectory = join(options.dir, "backend/views");
+
+//     mkdirSync(targetDirectory, { recursive: true });
+//     for (const fileName of readdirSync(sourceDirectory)) {
+//       copyFileSync(
+//         join(sourceDirectory, fileName),
+//         join(targetDirectory, fileName),
+//       );
+//     }
+//   },
+// });
+
+// // https://vitejs.dev/config
+// export default defineConfig({
+//   plugins: [copyViews()],
+// });
